4835eda3dfa93bc3e9f92f67e450eab2ee938832
AS-99-Parkinson <Apophis-99@users.noreply.github.com>
2022-10-23 23:35:00 +0100

    Fixed CollapseIcon.png not showing at runtime | Collapse Icon rotates to correct orientation depending on the panel

--- CFABingo/Controls/PanelHeader.xaml.cs
+++ CFABingo/Controls/PanelHeader.xaml.cs
@@ -1,10 +1,16 @@
-﻿using System.Windows.Controls;
+﻿using System;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Media.Imaging;
 
 namespace CFABingo.Controls;
 
 public partial class PanelHeader
 {
+    private bool _collapsed = false;
+    
     private string _title = "<PanelTitle>";
+    private Orientation _orientation = Orientation.Vertical;
 
     public string Title
     {
@@ -15,10 +21,42 @@ public partial class PanelHeader
             TitleText.Text = _title;
         }
     }
-    public Orientation Orientation { get; set; }
+    public Orientation Orientation
+    {
+        get => _orientation;
+        set { _orientation = value; UpdateOrientation(); }
+    }
+
+    public bool Collapsed
+    {
+        get => _collapsed;
+        set { _collapsed = value; UpdateOrientation(); }
+    }
 
     public PanelHeader()
     {
         InitializeComponent();
+    }
+
+    private void UpdateOrientation()
+    {
+        BitmapImage bitmapImage = new();
+        
+        bitmapImage.BeginInit();
+        bitmapImage.UriSource = new Uri("/Assets/CollapseIcon.png", UriKind.Relative);
+        
+        if (_orientation == Orientation.Horizontal)
+            bitmapImage.Rotation = _collapsed ? Rotation.Rotate90 : Rotation.Rotate270;
+        else
+            bitmapImage.Rotation = _collapsed ? Rotation.Rotate180 : Rotation.Rotate0;
+        
+        bitmapImage.EndInit();
+
+        Icon.Source = bitmapImage;
+    }
+
+    private void CollapseButtonOnClick(object sender, RoutedEventArgs e)
+    {
+        Collapsed = !Collapsed;
     }
 }
